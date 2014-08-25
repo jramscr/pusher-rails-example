3465a0414243ea2f003d353e63c555fbcbfed78a
jramscr <jramscr@gmail.com>
2014-08-25 16:27:07 -0600

    Schema changed. No progress on showing and storing messages.

--- app/controllers/pusher_controller.rb
+++ app/controllers/pusher_controller.rb
@@ -1,13 +1,19 @@
 class PusherController < ApplicationController
   def auth
-    user_id = (1..100).to_a.shuffle.first
+    user_id = (1..10).to_a.shuffle.first
     response = Pusher[params[:channel_name]].authenticate(params[:socket_id], {
       :user_id => user_id,
-      :user_message => "Sended Message",
+      :user_message => "#{""}",
       :user_info => {
         :name => "User #{user_id}"
       }
     })
     render :json => response
   end
+
+  def post
+    console.log("Hey mae")
+  end
+
+
 end
