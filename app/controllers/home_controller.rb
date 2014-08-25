739321bc64a46225915caea5c7b0fc9276a5b04c
jramscr <jramscr@gmail.com>
2014-08-25 14:51:04 -0600

    Migration of the Database and Model

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+  	#Cargar los datos guardados en la base de datos (Los mensajes almacenados.)
   end
 end
--- app/controllers/pusher_controller.rb
+++ app/controllers/pusher_controller.rb
@@ -1,8 +1,9 @@
 class PusherController < ApplicationController
   def auth
-    user_id = (1..10).to_a.shuffle.first
+    user_id = (1..100).to_a.shuffle.first
     response = Pusher[params[:channel_name]].authenticate(params[:socket_id], {
       :user_id => user_id,
+      :user_message => "Sended Message",
       :user_info => {
         :name => "User #{user_id}"
       }
